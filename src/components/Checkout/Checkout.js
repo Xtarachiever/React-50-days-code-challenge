85a16c539061e62dd405b538b2b63c5c656ee22c
Xtarachiever <estheradeyemi796@gmail.com>
2022-07-13 17:31:02 +0100

    Added the checkout page

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Advert from '../../images/amazon-ads.jpeg';
+import './Checkout.css'
 
 function Checkout() {
   return (
-    <div>
-        Checkout
-    </div>
+        <div className="checkout_container">
+            <div className="advert"> 
+               <img src={Advert} alt="Advert"/>
+            </div>
+            <div className="price">
+                <p>Subtotal (2 items) $23.92</p>
+                <div>
+                    <input type="checkbox"/>
+                    <span>This order contains a gift</span>
+                </div>
+                <button>Proceed to Checkout</button>
+            </div>
+        </div>
   )
 }
 
