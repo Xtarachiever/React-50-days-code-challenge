1c71c3e747300cf64e2d74199bfe13e794795f43
Xtarachiever <estheradeyemi796@gmail.com>
2022-07-14 11:52:20 +0100

    Updated the navbar items using data from stateProvider

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import Amazon from '../../images/AMAZON.jpeg';
+import { useStateValue } from '../StateProvider';
 import './Products.css'
 
 function Products({id,title,price,rating}) {
+  const [state,dispatch] = useStateValue();
+  const addToCart = () =>{
+    dispatch({
+      type:'ADD_TO_CART',
+      item:{
+        id:id,
+        title:title,
+        price:price,
+        rating:rating
+      }
+    })
+  }
   return (
     <div className='card shadow-2xl'>
         <div className='product_info'>
             <p>{title}</p>
             <p>{price}</p>
             <div className="flex">{
-              Array(rating).fill().map((_,i)=>(<p>⭐</p>))
+              Array(rating).fill().map((_,i)=>(<p key={i}>⭐</p>))
             }</div>
         </div>
         <img src={Amazon} alt="Product" className='w-1/6'/>
-        <button>Add to Basket</button>
+        <button onClick={addToCart}>Add to Basket</button>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom';
+import reducer,{initialState} from './components/Reducer';
+import { StateProvider } from './components/StateProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <React.StrictMode>
-      <App />
-    </React.StrictMode>
+        <StateProvider reducer={reducer} initialState={initialState}>
+         <App />
+        </StateProvider>
+      </React.StrictMode>
   </Router>
 );
